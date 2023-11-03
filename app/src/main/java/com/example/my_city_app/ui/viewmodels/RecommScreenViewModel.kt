e9f78b038fd10d2af4de6283506bb323756b49c9
mchudz97 <mchudz97@gmail.com>
2023-11-03 21:30:39 +0100

    Recommendation View Model

--- app/src/main/java/com/example/my_city_app/ui/viewmodels/RecommScreenViewModel.kt
+++ app/src/main/java/com/example/my_city_app/ui/viewmodels/RecommScreenViewModel.kt
@@ -14,43 +14,188 @@ import com.example.my_city_app.data.model.Recommendation
 import com.example.my_city_app.data.repository.CityRepository
 import com.example.my_city_app.data.repository.RecommendationRepository
 import com.example.my_city_app.data.utils.Category
-import com.example.my_city_app.ui.screens.CATEGORY
 import com.example.my_city_app.ui.screens.CITY
-import kotlinx.coroutines.flow.SharingStarted
+import com.example.my_city_app.utils.ValidationError
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import java.util.Calendar
 
-
-data class RecommUiState(
-    val city: City,
-    val category: Category,
-    val recommendations: List<Recommendation>
-)
+sealed interface RecommUiState {
+    data class Intro(
+        val city: City,
+    ) : RecommUiState
+    data class Default(
+        val city: City,
+        val category: Category,
+        val recommendations: List<Recommendation>
+    ) : RecommUiState
+    data class Focusing(
+        val city: City,
+        val category: Category,
+        val recommendations: List<Recommendation>,
+        val focusedRecomm: Recommendation
+    ) : RecommUiState
+    data class Editing(
+        val city: City,
+        val category: Category,
+        val recommendations: List<Recommendation>,
+        val validationErrorBundle: Set<ValidationError>,
+        val draft: Recommendation
+    ) : RecommUiState
+}
 
 class RecommScreenViewModel(
     savedStateHandle: SavedStateHandle,
-    recommendationRepository: RecommendationRepository,
-    cityRepository: CityRepository
+    val recommendationRepository: RecommendationRepository,
+    val cityRepository: CityRepository,
 ) : ViewModel() {
-
-    val recommUiState: StateFlow<RecommUiState> = recommendationRepository
-        .searchRecommendationsStream(
-            checkNotNull(savedStateHandle[CITY]),
-            checkNotNull(savedStateHandle[CATEGORY])
-        ).map {
-            RecommUiState(
-                city = cityRepository.getCityStream(checkNotNull(savedStateHandle[CITY])).first(),
-                category = checkNotNull(savedStateHandle[CATEGORY]),
-                recommendations = it
-            )
-        }.stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(5_000),
-            initialValue = RecommUiState(City(name = ""), Category.OTHER, emptyList())
+    private val cityId: Int = checkNotNull(savedStateHandle[CITY])
+    private val _uiState: MutableStateFlow<RecommUiState> =
+        MutableStateFlow(
+            RecommUiState.Intro(City(name = ""))
         )
+    val uiState: StateFlow<RecommUiState> = _uiState.asStateFlow()
 
+    init{
+        toIntroState()
+    }
+    fun beginFocusing(recommendation: Recommendation) {
+        when(val state = _uiState.value) {
+            is RecommUiState.Intro -> { }
+            is RecommUiState.Default -> {
+                _uiState.value = RecommUiState.Focusing(
+                    city = state.city,
+                    category = state.category,
+                    recommendations = state.recommendations,
+                    focusedRecomm = recommendation
+                )
+            }
+            is RecommUiState.Focusing -> {
+                _uiState.value = RecommUiState.Focusing(
+                    city = state.city,
+                    category = state.category,
+                    recommendations = state.recommendations,
+                    focusedRecomm = recommendation
+                )
+            }
+            is RecommUiState.Editing -> {
+                viewModelScope.launch {
+                    recommendationRepository.getRecommendationStream(recommendation.id).collect{
+                        _uiState.value = RecommUiState.Focusing(
+                            city = state.city,
+                            category = state.category,
+                            recommendations = state.recommendations,
+                            focusedRecomm = it
+                        )
+                    }
+                }
+            }
+        }
+    }
+    fun beginEdit() {
+        when(val state = _uiState.value) {
+            is RecommUiState.Focusing -> {
+                _uiState.value = RecommUiState.Editing(
+                    city = state.city,
+                    category = state.category,
+                    recommendations = state.recommendations,
+                    draft = state.focusedRecomm.copy(),
+                    validationErrorBundle = emptySet()
+                )
+            }
+            else -> { }
+        }
+    }
+    fun toDefaultState(category: Category) {
+        viewModelScope.launch {
+            cityRepository.getCityStream(cityId).collect{ city ->
+                recommendationRepository.searchRecommendationsStream(
+                    idCity = cityId,
+                    category = category
+                ).collect{ recomms ->
+                    _uiState.value = RecommUiState.Default(
+                        city = city,
+                        category = category,
+                        recommendations = recomms
+                    )
+                }
+            }
+        }
+    }
+    fun toIntroState() {
+        viewModelScope.launch {
+            cityRepository.getCityStream(cityId).collect{
+                _uiState.value = RecommUiState.Intro(it)
+            }
+        }
+    }
+    suspend fun save() {
+        when(val state = _uiState.value) {
+            is RecommUiState.Editing -> {
+                val errorBundle = mutableSetOf<ValidationError>()
+                if(state.draft.title.isBlank())
+                    errorBundle.add(ValidationError.ValidationErrorTitle)
+                if(state.draft.rate == 0)
+                    errorBundle.add(ValidationError.ValidationErrorRate)
+                if(state.draft.description.isBlank())
+                    errorBundle.add(ValidationError.ValidationErrorDescription)
+
+                if(errorBundle.isEmpty()) {
+                    val stateUpdatedDate = state.copy(
+                        draft = state.draft.copy(
+                            dateModified = Calendar.getInstance().time
+                        )
+                    )
+                    recommendationRepository.updateRecommendation(stateUpdatedDate.draft)
+                    toDefaultState(state.category)
+                } else {
+                    _uiState.value = state.copy(validationErrorBundle = errorBundle)
+                }
+            }
+            else -> { }
+        }
+    }
+    suspend fun delete() {
+        when (val state = _uiState.value) {
+            is RecommUiState.Focusing -> {
+                recommendationRepository.deleteRecommendation(state.focusedRecomm)
+            }
+            else -> { }
+        }
+    }
+    fun updateDraft(
+        title: String? = null,
+        rate: Int? = null,
+        description: String? = null,
+    ) {
+        when(val state = _uiState.value) {
+            is RecommUiState.Editing -> {
+                _uiState.value = state.copy(
+                    draft = state.draft.copy(
+                        title = title ?: state.draft.title,
+                        description = description ?: state.draft.description,
+                        rate = rate ?: state.draft.rate
+                    )
+                )
+            }
+            else -> { }
+        }
+    }
+    fun removeError(error: ValidationError) {
+        when(val state = _uiState.value) {
+            is RecommUiState.Editing -> {
+                _uiState.value = state.copy(
+                    validationErrorBundle = state.validationErrorBundle
+                        .filter { it != error }
+                        .toSet()
+                )
+            }
+            else -> { }
+        }
+
+    }
     companion object {
         val Factory: ViewModelProvider.Factory = viewModelFactory {
             initializer {
