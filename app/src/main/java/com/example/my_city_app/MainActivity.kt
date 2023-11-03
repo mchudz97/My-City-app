a12fa0790e344e4beaa8f262b8a0b80e3110e089
mchudz97 <mchudz97@gmail.com>
2023-11-03 22:12:36 +0100

    Navigation Graph + Activity update

--- app/src/main/java/com/example/my_city_app/MainActivity.kt
+++ app/src/main/java/com/example/my_city_app/MainActivity.kt
@@ -7,12 +7,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.my_city_app.ui.MyCityApp
 import com.example.my_city_app.ui.theme.MyCityappTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +25,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val windowSize = calculateWindowSizeClass(activity = this)
+                    MyCityApp(windowSize = windowSize.widthSizeClass)
                 }
             }
         }
---
+++ app/src/main/java/com/example/my_city_app/ui/screens/NavigationGraph.kt
@@ -0,0 +1,55 @@
+package com.example.my_city_app.ui.screens
+
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.example.my_city_app.data.utils.Category
+
+const val ROUTE_HOME = "Home"
+const val CITY = "City"
+const val ROUTE_CATEGORY = "Category"
+const val CATEGORY = "CatArg"
+const val ROUTE_CITY_CATEGORIES = "$ROUTE_CATEGORY/{$CITY}"
+const val ROUTE_CITY_CATEGORY = "$ROUTE_CITY_CATEGORIES/{$CATEGORY}"
+const val ROUTE_RECOMM_CREATE = "$ROUTE_CITY_CATEGORY/Create"
+
+@Composable
+fun MyCityNavHost(
+    windowSize: WindowWidthSizeClass,
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(navController = navController, startDestination = ROUTE_HOME, modifier = modifier) {
+        composable(route = ROUTE_HOME) {
+            HomeScreen(navigateToCategories = navController::navigate)
+        }
+        composable(route = ROUTE_CITY_CATEGORIES, arguments = listOf(navArgument(CITY){
+            type = NavType.IntType
+        })
+        ) {
+            RecommendationScreen(
+                onBackClicked = navController::popBackStack,
+                onCreateClick = navController::navigate,
+                windowMode = windowSize,
+            )
+        }
+        composable(
+            route = ROUTE_RECOMM_CREATE,
+            arguments = listOf(
+                navArgument(CITY) {
+                    type = NavType.IntType
+                },
+                navArgument(CATEGORY) {
+                    type = NavType.EnumType(type = Category::class.java)
+                }
+            )
+        ) {
+            RecommCreateView(onBack = navController::popBackStack)
+        }
+    }
+}
